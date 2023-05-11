84fd16fa56cbd81c9cc36a5e6ce6764dfce7c3f6
FernandoFGFUGR <ferfuentguerr@ugr.es>
2023-05-11 11:12:58 +0200

    receiver

---
+++ ps1/receiver.ps1
@@ -0,0 +1,38 @@
+# Importar el módulo WinSCP
+Add-Type -Path "C:\Program Files (x86)\WinSCP\WinSCPnet.dll"
+
+# Configurar la sesión de conexión a SFTP
+$sessionOptions = New-Object WinSCP.SessionOptions
+$sessionOptions.Protocol = [WinSCP.Protocol]::Sftp
+$sessionOptions.HostName = "150.214.24.215"
+$sessionOptions.UserName = "root"
+$sessionOptions.Password = "SiPMUGR128"
+$sessionOptions.SshHostKeyFingerprint = "ssh-rsa 2048 Wdajn7XKu2Fsbbx3xorcUiMoB+Qbi+GdYPDMGt5w6hk"
+
+
+# Iniciar la sesión de WinSCP
+$session = New-Object WinSCP.Session
+$session.Open($sessionOptions)
+
+# Especificar la carpeta remota y local
+$remotePath = "/swap"
+$localPath = "C:\Users\Usuario\Desktop\Laboratorio\Programacion-Automatizacion\ps1\Swap"
+
+# Enviar el archivo al servidor remoto
+$transferOptions = New-Object WinSCP.TransferOptions
+$transferResult = $session.GetFiles($remotePath, $localPath, $False, $transferOptions)
+
+# Comprobar si ha ocurrido algún error en la transferencia
+if ($transferResult.IsSuccess)
+{
+    Write-Host "Los archivos han sido recibidos correctamente."
+}
+else
+{
+    Write-Host "Ha ocurrido un error al transferir los archivos:" $transferResult.Failures[0].Message
+}
+
+# Cerrar la sesión de WinSCP
+$session.Dispose()
+
+Read-Host -Prompt "Presione Enter para salir"
